09663d6cbd5997bd0c0bbd19019f414eb7d7fb0b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-01 18:47:32 +0600

    Component Scan

--- src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
+++ src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
@@ -1,25 +1,17 @@
-package demo;
+package demo.kz.zhelezyaka.springboot;
 
 import demo.kz.zhelezyaka.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("demo.kz.zhelezyaka")
 public class DemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
         GreetingController controller = (GreetingController) context.getBean("greetingController");
         controller.sayHello();
-//        InjectedByConstructorService constructorService =
-//                (InjectedByConstructorService) context.getBean("injectedByConstructorService");
-//        constructorService.getMessage();
-//
-//        System.out.println("----------------------");
-//        SetterBasedService setterBasedService =
-//                (SetterBasedService) context.getBean("setterBasedService");
-//        setterBasedService.getMessage();
-
-
     }
 }
