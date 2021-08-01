cf9dd749b28e68a8cfe9eb9e63aa64911286b451
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-02 00:21:28 +0600

    Spring XML configuration with Spring Boot

--- src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
+++ src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("demo.kz.zhelezyaka")
+//@ComponentScan("demo.kz.zhelezyaka")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
