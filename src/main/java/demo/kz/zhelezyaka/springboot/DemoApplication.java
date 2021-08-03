ccd15c5500f815ba03b4544b0f37afecf7d304b0
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-03 22:26:49 +0600

    Importing XML configuration files

--- src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
+++ src/main/java/demo/kz/zhelezyaka/springboot/DemoApplication.java
@@ -4,7 +4,6 @@ import demo.kz.zhelezyaka.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
---
+++ src/test/java/kz/zhelezyaka/RussianIntegrationTest.java
@@ -0,0 +1,24 @@
+package kz.zhelezyaka;
+
+import demo.kz.zhelezyaka.services.HelloSpringServiceRussianImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/hello-spring.xml"})
+public class RussianIntegrationTest {
+
+    @Autowired
+    HelloSpringServiceRussianImpl helloSpringServiceRussian;
+
+    @Test
+    public void testHelloSpring() {
+        String greeting = helloSpringServiceRussian.getGreeting();
+        assertEquals("Привет Спринг!", greeting);
+    }
+}
