bab8f678b8e86140e06e322eeb5cba2682e9e5d2
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-04 19:35:26 +0600

    Creating and Index page and running via Spring Boot

---
+++ src/main/java/springMVC/SpringMVCApplication.java
@@ -0,0 +1,11 @@
+package springMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMVCApplication.class, args);
+    }
+}
