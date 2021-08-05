8b74271ecb32e2a69b0a4a7dc8de48a833169729
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-06 00:22:54 +0600

    Display a Product

--- src/main/java/springMVC/controllers/ProductController.java
+++ src/main/java/springMVC/controllers/ProductController.java
@@ -3,6 +3,7 @@ package springMVC.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springMVC.services.ProductService;
 
@@ -19,5 +20,11 @@ public class ProductController {
     public String listProducts(Model model) {
         model.addAttribute("products", productService.listAllProducts());
         return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product";
     }
 }
