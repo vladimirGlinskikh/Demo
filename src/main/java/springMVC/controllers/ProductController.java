f7311ac88b27a35b10ff088afe079e7d195dedf1
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-05 21:41:32 +0600

    Listing Products

---
+++ src/main/java/springMVC/controllers/ProductController.java
@@ -0,0 +1,23 @@
+package springMVC.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springMVC.services.ProductService;
+
+@Controller
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+}
---
+++ src/main/java/springMVC/services/ProductService.java
@@ -0,0 +1,9 @@
+package springMVC.services;
+
+import springMVC.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+}
