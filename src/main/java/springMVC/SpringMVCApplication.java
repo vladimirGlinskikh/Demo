ab62ebdc70d06ea0a4e3feb83b4552b9167aadd0
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-12 20:09:42 +0600

    Maven Dependencies

--- src/main/java/springMVC/SpringMVCApplication.java
+++ src/main/java/springMVC/SpringMVCApplication.java
@@ -2,10 +2,18 @@ package springMVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringMVCApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringMVCApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringMVCApplication.class, args);
+
+        System.out.println("Beans -------------");
+        System.out.println(ctx.getBeanDefinitionCount());
+
+        for (String name : ctx.getBeanDefinitionNames()){
+            System.out.println(name);
+        }
     }
 }
