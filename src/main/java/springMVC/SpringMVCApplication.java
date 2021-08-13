90313f27c504d8c73b22a619057a241132a80de5
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-13 21:29:06 +0600

    JPA Entities

--- src/main/java/springMVC/SpringMVCApplication.java
+++ src/main/java/springMVC/SpringMVCApplication.java
@@ -2,18 +2,10 @@ package springMVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringMVCApplication {
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(SpringMVCApplication.class, args);
-
-        System.out.println("Beans -------------");
-        System.out.println(ctx.getBeanDefinitionCount());
-
-        for (String name : ctx.getBeanDefinitionNames()){
-            System.out.println(name);
-        }
+        SpringApplication.run(SpringMVCApplication.class, args);
     }
 }
