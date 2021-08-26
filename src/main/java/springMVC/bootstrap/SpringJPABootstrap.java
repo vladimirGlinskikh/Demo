bc7b66ce88a040d43158e33fffb8dff2de46a64d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-26 06:46:22 +0600

    Bootstrap Data

---
+++ src/main/java/springMVC/bootstrap/SpringJPABootstrap.java
@@ -0,0 +1,58 @@
+package springMVC.bootstrap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import springMVC.domain.Product;
+import springMVC.services.ProductService;
+
+import java.math.BigDecimal;
+
+@Component
+public class SpringJPABootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        loadProducts();
+    }
+
+    public void loadProducts() {
+        Product product1 = new Product();
+        product1.setDescription("Product 1");
+        product1.setPrice(new BigDecimal("14.88"));
+        product1.setImageUrl("http://example.com/product1");
+        productService.saveOrUpdate(product1);
+
+        Product product2 = new Product();
+        product2.setDescription("Product 2");
+        product2.setPrice(new BigDecimal("12.68"));
+        product2.setImageUrl("http://example.com/product2");
+        productService.saveOrUpdate(product2);
+
+        Product product3 = new Product();
+        product3.setDescription("Product 3");
+        product3.setPrice(new BigDecimal("23.87"));
+        product3.setImageUrl("http://example.com/product3");
+        productService.saveOrUpdate(product3);
+
+        Product product4 = new Product();
+        product4.setDescription("Product 4");
+        product4.setPrice(new BigDecimal("144.23"));
+        product4.setImageUrl("http://example.com/product4");
+        productService.saveOrUpdate(product4);
+
+        Product product5 = new Product();
+        product5.setDescription("Product 5");
+        product5.setPrice(new BigDecimal("64.79"));
+        product5.setImageUrl("http://example.com/product5");
+        productService.saveOrUpdate(product5);
+    }
+}
