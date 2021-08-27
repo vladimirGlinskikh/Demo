145dd678c7ecc3e15c5b595a9c6d98a35ddcd9d9
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-27 14:41:48 +0600

    JPA Integration testing

---
+++ src/test/java/springMVC/config/JpaIntegrationConfig.java
@@ -0,0 +1,12 @@
+package springMVC.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("springMVC")
+public class JpaIntegrationConfig {
+}
---
+++ src/test/java/springMVC/services/ProductServiceJpaDaoImplTest.java
@@ -0,0 +1,30 @@
+package springMVC.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springMVC.config.JpaIntegrationConfig;
+import springMVC.domain.Product;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = JpaIntegrationConfig.class)
+@ActiveProfiles("jpadao")
+public class ProductServiceJpaDaoImplTest {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Test
+    public void testListMethod() {
+        List<Product> products = (List<Product>) productService.listAll();
+        assert products.size() == 5;
+    }
+}
